70596f502aade16e00dd0a24a7a391f9a428e68e
Nick Reid <reidnickb@gmail.com>
2017-10-30 13:32:48 -0400

    Adds data/seed file - removes distance_from_trail for stop model validation (this will be done on the StopResource join table) - reads stops_data.csv to seed database - adds migration to add to :stops => to_next_point (float), town_access (string), miles_from_k (float) and REMOVE distance_from_trail - adds stops_data.csv to /db/

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'csv'
+
+csv_text = File.read(Rails.root.join('db', 'stops_data.csv'))
+csv = CSV.parse(csv_text, headers: true, encoding: 'ISO-8859-1')
+
+csv.each do |row|
+  t = Stop.new
+  t.miles_from_ga = row['miles_from_ga']
+  t.to_next_point = row['to_next_point']
+  t.name = row['name']
+  t.town_access = row['town_access']
+  t.elevation = row['elevation']
+  t.miles_from_k = row['miles_from_k']
+  t.save
+  puts "#{t.miles_from_ga} - #{t.name} saved"
+end
+
+puts "There are now #{Stop.count} rows in the stop table"
