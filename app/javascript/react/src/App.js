c04b1833d776d5f369620186b7c22553bae8dfc9
Nick Reid <reidnickb@gmail.com>
2017-11-02 08:24:40 -0400

    Adds React router

--- app/javascript/react/src/App.js
+++ app/javascript/react/src/App.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import { Router, browserHistory, Route, IndexRoute } from 'react-router';
 import StopsIndexContainer from './containers/StopsIndexContainer'
+import StopsShowContainer from './containers/StopsShowContainer'
 
 const App = props => {
   return(
-    <StopsIndexContainer />
+    <Router history={browserHistory}>
+      <Route path='/stops'>
+        <IndexRoute component={StopsIndexContainer}/>
+        <Route path='/stops/:id' component={StopsShowContainer}/>
+      </Route>
+    </Router>
   )
 }
 
